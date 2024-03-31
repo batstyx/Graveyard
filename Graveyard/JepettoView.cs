f3f348c9a698f95d1c982b5eafb7199c3205cb40
Batian Styx <batstyx@gmail.com>
2024-03-31 17:53:54 +0100

    Add Joymancer Jepetto view

---
+++ Graveyard/JepettoView.cs
@@ -0,0 +1,21 @@
+﻿using Hearthstone_Deck_Tracker.API;
+using static HearthDb.CardIds.Collectible;
+
+namespace HDT.Plugins.Graveyard
+{
+    internal class JepettoView
+    {
+        private static ViewConfig _Config;
+        internal static ViewConfig Config
+        {
+            get => _Config ?? (_Config = new ViewConfig(Neutral.JoymancerJepetto)
+            {
+                CreateView = () => new NormalView(),
+                UpdateOn = GameEvents.OnPlayerPlay,
+                Condition = card => card.Type == "Minion" 
+                    && (card.Health == 1 
+                    || card.Attack == 1)
+            });
+        }
+    }
+}
